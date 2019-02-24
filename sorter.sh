df31e3e113801d59a8c3c05ae2ef25dad4826cf8
knuxfanwin8 <knuxfanwin8.mail@gmail.com>
2019-02-24 16:58:50 +0100

    clean up the code

--- sorter.sh
+++ sorter.sh
@@ -8,7 +8,7 @@
 
 usage="sorter.sh (refresh rate in seconds) (screenshot directory)"
 
-# Check if all variables are supplied
+# Check if all arguments are supplied
 if [ -z "$*" ]; then
     echo "No arguments supplied!"
     echo "Usage: $usage" 
@@ -21,62 +21,58 @@ if [ -z "$2" ]; then
     exit 1
 fi
 
-dirpath="$2"
+# Assign the arguments to variables
 secsleep="$1"
+dirpath="$2"
 
 # Check if the directory is valid
-
 if ! [ -e "$dirpath" ]; then
     echo "Screenshot directory does not exist!"
     exit 2
 fi
 
-# If screenshots directory doesn't exist, create it
+# If the screenshots sub-directory doesn't exist, create it
 if ! [ -d "$dirpath/Screenshots" ]; then mkdir "$dirpath/Screenshots"; fi
 
 # TODO: loop through existing screenshots to put them in the directory as well
-# For now we just move the files to a different directory so that the script doesn't go rampage on them
-if ! [ -d "$dirpath/oldImages" ]; then 
-    mkdir "$dirpath/oldImages"
-    for tomove in "$dirpath/"*; do
-        if ! [ "$tomove" = "Screenshots" ] && ! [ "$tomove" = "oldImages" ]; then
-            mv "$tomove" "$dirpath/oldImages"
-        fi
-    done
-fi
 
+# Create initial lastcheck file
+if ! [ -e "$dirpath/Screenshots/.lastcheck" ]; then touch "$dirpath/Screenshots/.lastcheck"; fi
 
 # The actual loop
-if ! [ -e "$dirpath/Screenshots/.lastcheck" ]; then touch "$dirpath/Screenshots/.lastcheck"; fi
 while true; do
-    # Create a folder for the current date and month, if necesary /
+    # Create a folder for the current date and month, if necesary 
     if ! [ -e "$dirpath/Screenshots/$(date +%Y-%m)" ] 
     then 
         mkdir "$dirpath/Screenshots/$(date +%Y-%m)" 
     fi
-    # Get last screenshot number /
+    # Get last screenshot number 
     if ! [ -e "$dirpath/Screenshots/.lastnum" ]; then echo "0" > "$dirpath/Screenshots/.lastnum"; fi
     number=$(cat "$dirpath/Screenshots/.lastnum") 
     # Watch for new files
     for file in "$dirpath/"*; do
         if ! [ "$file" = "Screenshots" ] && ! [ -d "$file" ]; then
-                # Get file extention
-                extention="${file##*.}"
-                # Move the file
-                if [ "$(find "$file" -cnewer "$dirpath/lastcheck")" ]; then
-                    mv "$file" "$dirpath/Screenshots/$(date +%Y-%m)/Screenshot_$number.$extention"
-                    echo "$((number + 1 ))" > "$dirpath/Screenshots/.lastnum"
-                    if [ -x "$(command -v kdialog)" ]; then 
-                        kdialog --title "Screenshot saved!" --passivepopup \
+            # Get file extention
+            extention="${file##*.}"
+            # Move the file
+            if [ "$(find "$file" -cnewer "$dirpath/lastcheck")" ]; then
+                mv "$file" "$dirpath/Screenshots/$(date +%Y-%m)/Screenshot_$number.$extention"
+                # Update the last screenshot number
+                echo "$((number + 1 ))" > "$dirpath/Screenshots/.lastnum"
+                # Show notification (or echo to the command line, depending on command availability)
+                if [ -x "$(command -v kdialog)" ]; then 
+                    kdialog --title "Screenshot saved!" --passivepopup \
 "Saved as Screenshot_$number.$extention." 3
-                    elif [ -x "$(command -v zenity)" ]; then 
-                        zenity --notification --text="Saved as Screenshot_$number.$extention."
-                    else
-                        echo 'Photo saved.'
-                    fi
+                elif [ -x "$(command -v zenity)" ]; then 
+                    zenity --notification --text="Saved as Screenshot_$number.$extention."
+                else
+                    echo 'Photo saved.'
                 fi
+            fi
         fi
     done
+    # Update last check file
     touch "$dirpath/Screenshots/.lastcheck"
+    # Wait before refresh
     sleep "$secsleep"
 done
